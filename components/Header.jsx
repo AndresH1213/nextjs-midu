8cf7d33d00083dde3e2b70b387461f3a8b7bc2be
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-03 18:28:46 -0500

    Extracting Layout composed from Footer and Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,64 @@
 import Link from 'next/link'
+import { useState, useRef } from 'react'
 
 export default function Header() {
+  const [results, setResults] = useState([])
+  const searchRef = useRef();
+
+  const getValue = () => searchRef.current?.value;
+
+  const handleChange = (e) => {
+    if (!e.target.value) {
+      return setResults([])
+    }
+    const q = getValue()
+    fetch(`/api/search?q=${q}`)
+      .then(res => res.json())
+      .then(searchResults => {
+        setResults(searchResults)
+      })
+  }
   return <header className='flex justify-between items-center p-4 max-w-xl m-auto'>
-    <h1 className='font-bold'>next<span className='font-light'>xkcd</span></h1>
+    <h1 className='font-bold'>
+      <Link href='/'>
+        <a className='transition hover:opacity-80'>
+          next<span className='font-light'>xkcd</span>
+        </a>
+      </Link>
+    </h1>
     <nav>
       <ul className='flex flex-row gap-2'>
         <li><Link href='/'><a className='text-sm font-semibold'>Home</a></Link></li>
-        <li><Link href='/about'><a className='text-sm font-semibold'>About</a></Link></li>
-        <li><Link href='/search'><a className='text-sm font-semibold'>Search</a></Link></li>
+        <li>
+          <input className='px-4 py-1 text-xs border border-gray-400 rounded-3xl' ref={searchRef} type='search' 
+          onChange={handleChange}/>
+          <div className='relative z-10'>
+            {
+              Boolean(results.length) && <div className='absolute bg-white top-0 left-0'>
+                <ul className='w-full overflow-hidden border rounded-lg shadow-xl border-gray-50'>
+                  <li className='m-0' key='all-results'>
+                    <Link href={`/search?q=${getValue()}`}>
+                      <a className='block italic text-gray-400 px-2 py-1 overflow-hidden text-sm font-semibold
+                      hover:bg-slate-200 text-ellipsis whitespace-nowrap'>Ver {results.length} resultados</a>
+                    </Link>
+                  </li>
+                  {
+                    results.map(result => {
+                      return (
+                        <li className='m-0' key={result.id}>
+                          <Link href={`/comic/${result.id}`}>
+                            <a className='block px-2 py-1 overflow-hidden text-sm font-semibold
+                            hover:bg-slate-200 text-ellipsis whitespace-nowrap'>{result.title}</a>
+                          </Link>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </div>
+            }
+          </div>
+        </li>
       </ul>
     </nav>
   </header>
