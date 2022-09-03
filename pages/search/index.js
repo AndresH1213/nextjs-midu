e1e76a1e206140cb172065990bc9fcd3b355195e
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-03 18:35:41 -0500

    adding component that shows the result of the search

---
+++ pages/search/index.js
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import { Layout } from 'components/Layout';
+import Link from 'next/link';
+import Image from 'next/image';
+import { search } from 'services/search';
+
+export default function Component({ query, results }) {
+  return (
+    <>
+      <Head>
+        <title>xkcd - Results for queries</title>
+        <meta name="description" content={`Search results for ${query}`} />
+      </Head>
+
+      <Layout>
+        <h1>
+          {results.legth} Resultados para {query}
+        </h1>
+        {results.map((result) => {
+          return (
+            <Link key={result.id} href={`/comic/${result.id}`}>
+              <a className="flex flex-row bg-slate-300 hover:bg-slate-50 content-center">
+                <Image
+                  width="50"
+                  height="50"
+                  alt={result.alt}
+                  src={result.img}
+                  className="rounded-full"
+                />
+                <div>
+                  <h2>{result.title}</h2>
+                </div>
+              </a>
+            </Link>
+          );
+        })}
+      </Layout>
+      <style jsx>{``}</style>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { q = '' } = query;
+
+  const { results } = await search({ query: q });
+
+  return {
+    props: {
+      query: q,
+      results,
+    },
+  };
+}
