dbf972b8072b9be9a70c994a0b77e6bf89f22125
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-06 20:36:43 -0500

    main page implement i18n hook

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 import fs from 'node:fs/promises';
 import { Layout } from 'components/Layout';
+import { useI18N } from 'context/i18n';
 
 export default function Home({ latestComics }) {
+  const { t } = useI18N();
+
   return (
     <div>
       <Head>
-        <title>xkcd - Comiscs for developers</title>
         <meta name="description" content="Comics for developers" />
       </Head>
 
       <Layout>
-        <h2 className="text-3xl font-bold text-center mb-10">Latest Comics</h2>
+        <h2 className="text-3xl font-bold text-center mb-10">
+          {t('LATEST_COMICS')}
+        </h2>
         <section
           className="grid grid-cols-1 gap-2 max-w-md m-auto sm:grid-cols-2 
         md:grid-cols-3"
