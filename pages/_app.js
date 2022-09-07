e7c33d33c4f32fe68092869689bb352186f0707f
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-06 20:37:26 -0500

    wrapping the app with the I18N provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
 import { NextUIProvider } from '@nextui-org/react';
+import { I18NProvider, useI18N } from 'context/i18n';
 import Head from 'next/head';
 import '../styles/globals.css';
+
+const DefaultHeadApp = () => {
+  const { t } = useI18N();
+  return (
+    <Head>
+      <title>{t('SEO_DEFAULT_TITLE')}</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
+      <I18NProvider>
+        <DefaultHeadApp />
+        <Component {...pageProps} />
+      </I18NProvider>
     </NextUIProvider>
   );
 }
